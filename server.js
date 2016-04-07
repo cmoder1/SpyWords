13e051ad26da2aaaee114c2f2f9b80b4d7d4c288
cmoder1 <charles_moder@brown.edu>
2016-04-07 18:40:46 -0400

    Connect to MongoDB database with mongoose

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ var express = require('express');
 var	bodyParser = require('body-parser');
 var anyDB = require('any-db');
 var engines = require('consolidate');
+var mongoose = require('mongoose');
 
 // Prepare variables
 var conn = anyDB.createConnection('sqlite3://codenames.db');
 var app = express(); 
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
-// =================
 
 app.engine('html', engines.hogan); // tell Express to run .html files through Hogan
 app.set('views', __dirname + '/templates'); // tell Express where to find templates
@@ -22,6 +22,99 @@ app.use(bodyParser.json());
 // allow access to the scipts and styles for the webpages
 app.use('/scripts', express.static('scripts'));
 app.use('/styles', express.static('styles'));
+
+
+
+/* ========================================================
+ * ==================  MongoDB Database  ==================
+ * ======================================================== */
+
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function() {
+    // Create your schemas and models here
+    console.log("Connected to DB!");
+
+    /* ========================================================
+     * ================  Build Schema/Model  ==================
+     * ======================================================== */
+
+    var gameSchema = new mongoose.Schema({
+        gameID: String,
+        cards: [{ word: String, team: String, guessed: Boolean }],
+        players: [{ name: String, team: String, role: String }],
+        turn: String,
+        gameOver: Boolean
+    });
+
+    var Game = mongoose.model('Game', gameSchema);
+
+    /* ========================================================
+     * ================  Socket Communications  ===============
+     * ======================================================== */
+
+    io.sockets.on('connection', function(socket){
+        // clients emit this when they join new rooms
+        socket.on('join', function(){
+            console.log('TODO');
+        });
+
+        // the client disconnected/closed their browser window
+        socket.on('disconnect', function(){
+            // Leave the room!
+            console.log('TODO');
+        });
+
+    });
+
+    /* ========================================================
+     * ================  Access Home/Chatroom  ================
+     * ======================================================== */
+
+    // URL to access a specified chatroom
+    app.get('/game', function(request, response){
+        // Send the user to a game
+        generateWords(function(words) {
+
+            var teams = assignCards();
+            var card_data = { 'cards' : [] };
+
+            for (var i=0; i<words.length; i++) {
+                card_data['cards'].push({ 'word' : words[i], 'team' : teams[i] });
+            }
+            response.render('mock_game.html', card_data);
+        });
+    });
+
+    // Catchall page - Home
+    app.get('*', function(request, response){
+        // Display the home screen
+        response.render('mock_home.html');
+    });
+
+
+    // changed from *app*.listen(8080);
+    // to server.listen(8080);
+
+    // Set up the listener and make the table of messages once done
+    server.listen(8080, function() {
+        /*conn.query('CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT,'+
+            ' room TEXT, nickname TEXT, body TEXT, time INTEGER);')
+        .on('end', function(){
+            console.log('Messages table set');
+        });
+
+        conn.query('CREATE TABLE IF NOT EXISTS rooms (roomID TEXT PRIMARY KEY, roomName TEXT);')
+            .on('end', function(){
+                console.log('Rooms table set');
+            });
+        */
+    });
+
+
+});
+
+mongoose.connect('mongodb://cmoder:puddleglum@ds019940.mlab.com:19940/codenames');
 
 
 /* ========================================================
@@ -98,66 +191,4 @@ function assignCards() {
 
     return assignments;
 }
-
-/* ========================================================
- * ================  Socket Communications  ===============
- * ======================================================== */
-
-io.sockets.on('connection', function(socket){
-    // clients emit this when they join new rooms
-    socket.on('join', function(){
-        console.log('TODO');
-    });
-
-    // the client disconnected/closed their browser window
-    socket.on('disconnect', function(){
-        // Leave the room!
-        console.log('TODO');
-    });
-
-});
-
-/* ========================================================
- * ================  Access Home/Chatroom  ================
- * ======================================================== */
-
-// URL to access a specified chatroom
-app.get('/game', function(request, response){
-    // Send the user to a game
-    generateWords(function(words) {
-
-        var teams = assignCards();
-        var card_data = { 'cards' : [] };
-
-        for (var i=0; i<words.length; i++) {
-            card_data['cards'].push({ 'word' : words[i], 'team' : teams[i] });
-        }
-        response.render('mock_game.html', card_data);
-    });
-});
-
-// Catchall page - Home
-app.get('*', function(request, response){
-    // Display the home screen
-    response.render('mock_home.html');
-});
-
-
-// changed from *app*.listen(8080);
-// to server.listen(8080);
-
-// Set up the listener and make the table of messages once done
-server.listen(8080, function() {
-	/*conn.query('CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT,'+
-		' room TEXT, nickname TEXT, body TEXT, time INTEGER);')
-	.on('end', function(){
-		console.log('Messages table set');
-	});
-
-	conn.query('CREATE TABLE IF NOT EXISTS rooms (roomID TEXT PRIMARY KEY, roomName TEXT);')
-		.on('end', function(){
-			console.log('Rooms table set');
-		});
-    */
-});
 
